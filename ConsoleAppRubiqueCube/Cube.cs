13f2709e1d4feee0342e098c59d085657d0c8bc2
Erwan <55313521+ErwanExplorer@users.noreply.github.com>
2026-02-02 13:53:40 +0100

    Refactor Cube properties to use Face objects and initialize with colors

--- ConsoleAppRubiqueCube/Cube.cs
+++ ConsoleAppRubiqueCube/Cube.cs
@@ -2,15 +2,21 @@
 
 public class Cube
 {
-    public string Top { get; set; }
-    public string Bottom { get; set; }
-    public string Left { get; set; }
-    public string Right { get; set; }
-    public string Front { get; set; }
-    public string back { get; set; }
-
+    public Face Top { get; set; }
+    public Face Bottom { get; set; }
+    public Face Left { get; set; }
+    public Face Right { get; set; }
+    public Face Front { get; set; }
+    public Face back { get; set; }
+    
     public Cube()
     {
-      
+        Top = new Face("Blanc", "Top");
+        Bottom = new Face("Jaune", "Bottom");
+        Left = new Face("Bleu", "Left");
+        Right = new Face("Vert", "Right");
+        Front = new Face("Rouge", "Front");
+        back = new Face("Orange", "Back");
     }
+    
 }
